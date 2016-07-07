e54b723e713d98906ced9571e061ac529477413d
Michael Burkhardt <p.michael.b@t-online.de>
2016-07-07 22:12:46 +0200

    Neue Version alpha 05. Fraktion gegen LOP ausgetauscht, Basis verschoben, DB-Logistik eingebaut

--- Projekte/[J]Insurgency_Alpha05.Kunduz/scripts/define_templates.hpp
+++ Projekte/[J]Insurgency_Alpha05.Kunduz/scripts/define_templates.hpp
@@ -1,6 +1,6 @@
 #define JGKP_var_template_small_grp ["JGKP_T8_smallGrp1", "JGKP_T8_smallGrp2", "JGKP_T8_smallGrp3", "JGKP_T8_smallGrp4"]
 #define JGKP_var_template_big_grp 	["JGKP_T8_bigGrp1", "JGKP_T8_bigGrp2", "JGKP_T8_bigGrp3"]
 #define JGKP_var_template_motorized ["JGKP_T8_motGrp1", "JGKP_T8_motGrp2", "JGKP_T8_motGrp3"]
-#define JGKP_var_template_car 		["JGKP_T8_car1", "JGKP_T8_car2", "JGKP_T8_car3"]
-#define JGKP_var_template_static 	["JGKP_T8_stat1", "JGKP_T8_stat2", "JGKP_T8_stat3", "JGKP_T8_stat4", "JGKP_T8_stat5"]
+#define JGKP_var_template_car 		["JGKP_T8_car1", "JGKP_T8_car2", "JGKP_T8_car3", "JGKP_T8_car4", "JGKP_T8_car5", "JGKP_T8_car6"]
+#define JGKP_var_template_static 	["JGKP_T8_stat1", "JGKP_T8_stat2", "JGKP_T8_stat3", "JGKP_T8_stat4", "JGKP_T8_stat5", "JGKP_T8_stat6", "JGKP_T8_stat7", "JGKP_T8_stat8"]
 #define JGKP_var_template_armored 	["JGKP_T8_tank1", "JGKP_T8_tank2", "JGKP_T8_tank3", "JGKP_T8_tank4"]
